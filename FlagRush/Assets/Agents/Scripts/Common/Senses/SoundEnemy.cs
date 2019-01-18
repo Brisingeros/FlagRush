55669caedf353cbc99e9d5c5eddfe96a5303c1ac
Laura <lauritaninetyseven@gmail.com>
2019-01-18 13:48:53 +0100

    programados sentidos de aliados y enemigos

--- FlagRush/Assets/Agents/Scripts/Common/Senses/SoundEnemy.cs
+++ FlagRush/Assets/Agents/Scripts/Common/Senses/SoundEnemy.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class SoundEnemy : Sense {
 
-	void OnTriggerEnter(Collider other){
+    public Player pla;
+
+    private void Start()
+    {
+        pla = transform.parent.GetComponent<Player>();
+
+    }
+    void OnTriggerEnter(Collider other){
 	
 		Aspect aspect = other.GetComponent<Aspect> ();
 
 		if (aspect != null) {
 			
-			if (aspect.alive && aspect.teamAct != pla.teamAct)
-				Debug.Log ("Enemigo escuchado");
-			
-		}
+			if (aspect.alive && aspect.aspectAct == Aspect.aspect.Sound && aspect.teamAct != pla.teamAct)
+            {
+                pla.addSound(aspect);
+                Debug.Log("Enemigo escuchado");
+
+            }
+
+        }
 	
 	}
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        Aspect aspect = other.GetComponent<Aspect>();
+
+        if (aspect != null)
+        {
+
+            if (aspect.alive && aspect.aspectAct == Aspect.aspect.Sound && aspect.teamAct != pla.teamAct)
+            {
+                pla.removeSound(other.GetComponent<Aspect>());
+            }
+
+        }
+
+    }
+
+    void Update()
+    {
+        pla.OrderByDistance("sound");
+    }
 
 }
