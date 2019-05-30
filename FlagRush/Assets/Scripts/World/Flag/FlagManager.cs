51df10ccad199e402fea623985003eebdd29a1be
Laura <lauritaninetyseven@gmail.com>
2019-05-30 12:32:27 +0200

    arregladas animaciones de enfermeras reordenacion de carpetas

---
+++ FlagRush/Assets/Scripts/World/Flag/FlagManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagManager : MonoBehaviour {
+
+	public Team.team team;
+
+	void Start(){
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		Player p = other.GetComponent<Player> ();
+
+        if (p != null && p.aspectAct == Aspect.aspect.NPC && p.getTypeNpc() == TypeNPC.type.Soldier && p.teamAct != team)
+        {
+
+            UIController ui = FindObjectOfType<UIController>();
+            ui.setVictoryOnScreen(p.teamAct);
+            Invoke("changeScene", 3.0f);
+
+        }
+
+
+	}
+
+}
