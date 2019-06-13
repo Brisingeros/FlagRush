6a3da91fc7e6e9457362050f6939809bc5392b6c
Alvaro <alvarete212@gmail.com>
2019-06-13 02:28:04 +0200

    Sistema de gestion de efectos de sonido asi como de musica de fondo.

---
+++ FlagRush/Assets/Scripts/SFX Animations/EfectosSonidos.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectosSonidos : MonoBehaviour {
+
+    public AudioClip disparoSonido;
+    public AudioClip andarSonido;
+    public AudioClip muerteSonido;
+    public AudioClip alertaSonido;
+    public AudioClip resucitarSonido;
+    public AudioClip meleeSonido;
+    public AudioSource audioSource;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void melee()
+    {
+
+        audioSource.PlayOneShot(meleeSonido);
+
+
+    }
+
+
+    void disparo()
+    {
+
+        audioSource.PlayOneShot(disparoSonido);
+
+    }
+
+    void andar()
+    {
+
+        audioSource.PlayOneShot(andarSonido);
+
+    }
+
+    void muerte()
+    {
+
+        audioSource.PlayOneShot(muerteSonido);
+
+    }
+
+    void alerta()
+    {
+
+
+
+    }
+
+    void resucitar()
+    {
+
+        audioSource.PlayOneShot(resucitarSonido);
+
+    }
+}
---
+++ FlagRush/Assets/Sound/MUSIC/AleatorioNotDestroy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AleatorioNotDestroy : MonoBehaviour {
+
+    public AudioSource bckgMusic1;
+    public AudioSource bckgMusic2;
+    public AudioSource bckgMusic3;
+
+    int aleatorio;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        aleatoriedad();
+        if (aleatorio == 1)
+        {
+
+            bckgMusic1.Play();
+            bckgMusic2.Stop();
+            bckgMusic3.Stop();
+
+
+
+        }
+        else if (aleatorio == 2)
+        {
+
+            bckgMusic1.Stop();
+            bckgMusic2.Play();
+            bckgMusic3.Stop();
+
+        }
+        else if (aleatorio == 3)
+        {
+
+            bckgMusic1.Stop();
+            bckgMusic2.Stop();
+            bckgMusic3.Play();
+
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+
+
+    }
+
+    public void aleatoriedad()
+    {
+        aleatorio = Random.Range(1, 4);
+   
+    }
+}
