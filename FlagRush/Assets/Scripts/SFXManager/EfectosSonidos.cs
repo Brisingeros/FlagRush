2dbb16d1423a3d337002bcd21110c729bdd67fb6
Alvaro <alvarete212@gmail.com>
2019-06-19 23:05:21 +0200

    Actualizado sistema de sonidos y nuevo gestor de distancia de terreno

---
+++ FlagRush/Assets/Scripts/SFXManager/EfectosSonidos.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectosSonidos: MonoBehaviour {
+
+    public AudioClip disparoSonido;
+    public AudioClip andarSonidoIzq;
+    public AudioClip andarSonidoDrch;
+    public AudioClip muerteSonido;
+    public AudioClip alertaSonido;
+    public AudioClip resucitarSonido;
+    public AudioClip meleeSonido;
+    public AudioClip maniobraResucitar;
+    public AudioSource audioSource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void melee()
+    {
+
+        audioSource.PlayOneShot(meleeSonido);
+
+
+    }
+
+
+    void disparo()
+    {
+
+        audioSource.PlayOneShot(disparoSonido);
+
+    }
+
+    void andar_izq()
+    {
+
+        audioSource.PlayOneShot(andarSonidoIzq);
+
+    }
+
+    void andar_drch()
+    {
+
+        audioSource.PlayOneShot(andarSonidoDrch);
+
+    }
+
+    void muerte()
+    {
+
+        audioSource.PlayOneShot(muerteSonido);
+
+    }
+
+
+    void alerta()
+    {
+
+
+
+    }
+
+    void resucitar()
+    {
+
+        audioSource.PlayOneShot(resucitarSonido);
+
+    }
+
+    void revivir()
+    {
+        audioSource.PlayOneShot(maniobraResucitar);
+    }
+}
---
+++ FlagRush/Assets/Scripts/World/TerrainManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+
+    public Terrain m_terrain;  //referencia al terreno
+    public float DrawDistance; //distancia de visibilidad, poner a 5000 en el editor
+
+    // Use this for initialization
+    void Start()
+    {
+
+        m_terrain.detailObjectDistance = DrawDistance;
+
+    }
+
+    void update()
+    {
+
+        m_terrain.detailObjectDistance = DrawDistance;
+
+    }
+}
